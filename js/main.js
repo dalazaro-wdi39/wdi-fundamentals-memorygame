70d093c846b7b8ef1c636b8d16403a424916c45d
Daryl Jason Lazaro <dlazaro@Daryls-MacBook-Pro.local>
2017-06-20 16:48:43 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
- console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry, try again.");
+  }
+}
