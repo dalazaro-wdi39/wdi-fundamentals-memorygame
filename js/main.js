137b40767fe2d7529bab9593111ec1dbadcfb108
Daryl Jason Lazaro <dlazaro@Daryls-MacBook-Pro.local>
2017-06-21 22:38:45 -0700

    created game logic and flip cards

--- js/main.js
+++ js/main.js
@@ -1,44 +1,57 @@
 var cards = [
     {
-      rank: "queen",
-      suit: "hearts",
+      rank: 'queen',
+      suit: 'hearts',
       cardImage: "images/queen-of-hearts.png"
     },
     {
-      rank: "queen",
-      suit: "diamonds",
+      rank: 'queen',
+      suit: 'diamonds',
       cardImage: "images/queen-of-diamonds.png"
     },
     {
-      rank: "king",
-      suit: "hearts",
+      rank: 'king',
+      suit: 'hearts',
       cardImage: "images/king-of-hearts.png"
     },
     {
-      rank: "king",
-      suit: "diamonds",
+      rank: 'king',
+      suit: 'diamonds',
       cardImage: "images/king-of-diamonds.png"
     }
 ];
 
 var cardsInPlay = [];
 
-var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
-  cardsInPlay.push(cards[cardId].rank);
-// Should checkForMatch(); go here, and have "Sorry, try again" log after the first and second turn?
-};
-
 var checkForMatch = function () {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
+  if(cardsInPlay[0] === cardsInPlay[1]) {
       alert("You found a match!");
   } else {
       alert("Sorry, try again.");
   }
 };
 
-flipCard(0);
-flipCard(2);
-checkForMatch(); //or should it go here, and have "Sorry, try again" log after only the second turn?
+var flipCard = function () {
+  cardId = this.getAttribute('data-id');
+  console.log(cardId);
+  cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  if (cardsInPlay.length === 2) {
+      checkForMatch();
+      cardsInPlay = [];
+  }
+};
+
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+
+createBoard();
